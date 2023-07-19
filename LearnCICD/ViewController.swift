0a860a60a5b276475aed1cee63ef0dc9bbc76f84
nipaashra <nipa.ashra@gmail.com>
2023-07-19 12:29:09 +0800

    Init project

---
+++ LearnCICD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnCICD
+//
+//  Created by nipa on 19/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello world")
+    }
+
+
+}
+
